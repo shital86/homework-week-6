3bee6979995623f0b22810155df68ccf6357608f
Shital <shitalkakani86@gmail.com>
2022-03-06 13:57:13 +0000

    My homework week 6

---
+++ src/program7.java
@@ -0,0 +1,13 @@
+public class program7 {public static void main(String[] args) {
+
+            int a = 74;
+            int b = 36;
+            System.out.println("The sum of two numbers");
+            System.out.println(a + b);
+
+
+        }
+    }
+
+
+
---
+++ src/program8.java
@@ -0,0 +1,12 @@
+public class program8  {
+
+    public static void main(String[] args) {
+        int a = 74;
+        int b = 36;
+        System.out.println("The sum of two numbers");
+        System.out.println(a + b);
+
+
+    }
+}
+
